0db79983db19a2e132a542e041f803c84903ed9c
ibrahim-b-dev <ibrahim.dev.b@gmail.com>
2024-10-19 19:38:17 +0300

    1.6 - 1.7 - 1.8 - 1.9 - 1.10

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+const Header = ({ text }) => <h1>{text}</h1>;
+
+const Button = ({ text, handleClick }) => <button onClick={handleClick}>{text}</button>;
+
+const StatisticLine = ({ text, value }) => <p>{text}: {value}</p>
+
+  const Statistics = ({ values }) => {
+  const [good, neutral, bad] = values;
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100;
+  
+  if (all === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+
+  return (
+    <div>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive + " %"} />
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const increaseGood = () => {
+    const newValue = good + 1 
+    console.log(newValue)
+    setGood(newValue)
+  }
+
+  const increaseNeutral = () => {
+    const newValue = neutral + 1
+    setNeutral(newValue)
+  }
+  const increaseBad = () => {
+    const newValue = bad + 1
+    setBad(newValue)
+  }
+
+  return (
+    <div>
+      <Header text={"give feed back"}/>
+
+      <Button handleClick={increaseGood} text="good" />
+      <Button handleClick={increaseNeutral} text="neutral" />
+      <Button handleClick={increaseBad} text="bad" />
+
+      <Header text="statistics" />
+      <Statistics values={[good, neutral, bad]} />
+    </div>
+  )
+};
+
+export default App;
