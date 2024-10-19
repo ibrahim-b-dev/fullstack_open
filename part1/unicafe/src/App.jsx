bf60eacbb87c53354a519cff431c827c40ac7e66
ibrahim-b-dev <ibrahim.dev.b@gmail.com>
2024-10-19 19:48:11 +0300

    1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,7 +4,14 @@ const Header = ({ text }) => <h1>{text}</h1>;
 
 const Button = ({ text, handleClick }) => <button onClick={handleClick}>{text}</button>;
 
-const StatisticLine = ({ text, value }) => <p>{text}: {value}</p>
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
 
   const Statistics = ({ values }) => {
   const [good, neutral, bad] = values;
@@ -19,14 +26,16 @@ const StatisticLine = ({ text, value }) => <p>{text}: {value}</p>
   }
 
   return (
-    <div>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive + " %"} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive + " %"} />
+      </tbody>
+    </table>
   )
 }
 
@@ -36,8 +45,7 @@ const App = () => {
   const [bad, setBad] = useState(0);
 
   const increaseGood = () => {
-    const newValue = good + 1 
-    console.log(newValue)
+    const newValue = good + 1
     setGood(newValue)
   }
 
